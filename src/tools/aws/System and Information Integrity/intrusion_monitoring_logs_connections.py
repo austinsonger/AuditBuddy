9bfa3ddc8eebc70873a969ae2758f475832b95d2
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-06-11 13:55:52 -0500

    Update intrusion_monitoring_logs_connections.py

--- src/tools/aws/System and Information Integrity/intrusion_monitoring_logs_connections.py
+++ src/tools/aws/System and Information Integrity/intrusion_monitoring_logs_connections.py
@@ -1,7 +1,15 @@
 # main.py
 import sys
 import os
+"""
+NIST 800-53 Control Numbers:
+- AU-12: Audit Generation
+- SI-4: Information System Monitoring
 
+SOC 2 Control Numbers:
+- CC6.1: Security and Availability
+- CC7.1: System Monitoring
+"""
 # Adjust the Python path to include the parent directory of aws
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -43,13 +51,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-"""
-NIST 800-53 Control Numbers:
-- AU-12: Audit Generation
-- SI-4: Information System Monitoring
-
-SOC 2 Control Numbers:
-- CC6.1: Security and Availability
-- CC7.1: System Monitoring
-"""
