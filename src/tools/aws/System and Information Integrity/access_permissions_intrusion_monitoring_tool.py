15e685d8a8a1dc2c9670247694cb23ffe4b53bad
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-06-11 13:56:57 -0500

    Update access_permissions_intrusion_monitoring_tool.py

--- src/tools/aws/System and Information Integrity/access_permissions_intrusion_monitoring_tool.py
+++ src/tools/aws/System and Information Integrity/access_permissions_intrusion_monitoring_tool.py
@@ -1,6 +1,16 @@
 # main.py
 import sys
 import os
+"""
+NIST 800-53 Control Numbers:
+- AC-2: Account Management
+- AC-3: Access Enforcement
+- SI-4: Information System Monitoring
+
+SOC 2 Control Numbers:
+- CC6.1: Security and Availability
+- CC6.6: Logical and Physical Access Controls
+"""
 
 # Adjust the Python path to include the parent directory of aws
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -48,14 +58,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-"""
-NIST 800-53 Control Numbers:
-- AC-2: Account Management
-- AC-3: Access Enforcement
-- SI-4: Information System Monitoring
-
-SOC 2 Control Numbers:
-- CC6.1: Security and Availability
-- CC6.6: Logical and Physical Access Controls
-"""
